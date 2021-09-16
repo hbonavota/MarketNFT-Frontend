dfed7b0098672a2ed535a4de4dc9ef71a2bfeb4d
¨hbonavota¨ <hbonavota@gmail.com>
2021-09-17 00:01:20 +0200

    <modificated routes>[api backend heroku]modificate all routes

--- src/actions/MetaMaskTransaction.js
+++ src/actions/MetaMaskTransaction.js
@@ -2,15 +2,15 @@ import axios from "axios";
 import { TRANSACTION_METAMASK } from "./constants";
 
 export function TransactionMetaMask(eth) {
-    return async function(dispatch) {
-         try {
+   return async function (dispatch) {
+      try {
 
-           const response = await axios.post(`https://nft-e-commerce11.herokuapp.com/transaction`, eth)
-           console.log("Action Transaction Metamask:",response.data)
-            return dispatch({type: TRANSACTION_METAMASK, payload: response.data})
+         const response = await axios.post(`https://nft-e-commerce11.herokuapp.com/transaction`, eth)
+         console.log("Action Transaction Metamask:", response.data)
+         return dispatch({ type: TRANSACTION_METAMASK, payload: response.data })
 
-         } catch(error) {
-            console.log(error)
-         }
-    }
+      } catch (error) {
+         console.log(error)
+      }
+   }
 }
