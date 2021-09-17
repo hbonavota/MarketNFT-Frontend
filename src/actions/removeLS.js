220739dbb44903c2a799e43c73a282051c6bae9c
¨hbonavota¨ <hbonavota@gmail.com>
2021-09-17 15:01:25 +0200

    <add changes>[google login]changes in redirec

--- src/actions/removeLS.js
+++ src/actions/removeLS.js
@@ -1,6 +1,6 @@
 export function removeLS(e) {
    let itemsLS = JSON.parse(window.localStorage.getItem("user"))
-   let itemfilters = itemsLS?.filter(nft => nft._id != e._id )
+   let itemfilters = itemsLS?.filter(nft => nft._id !== e._id )
    if(itemfilters){
       window.localStorage.setItem("user",JSON.stringify(itemfilters));
    }
