2a60a596478a5f27d3320fd8bfab2c5a033d5181
¨hbonavota¨ <hbonavota@gmail.com>
2021-09-20 20:32:12 +0200

    <add modifications actions/addToDB>

--- src/actions/shoppingCart/addToDB.js
+++ src/actions/shoppingCart/addToDB.js
@@ -1,11 +1,13 @@
 import axios from "axios";
 
+
 export default function addToDB(payload) {
     return async function (dispatch) {
       try {
         let response = await axios.post("https://nft-e-commerce11.herokuapp.com/userShoppingCart",payload);
+
          return dispatch({
-            type: "DB_SHOPPING_CART",
+            type: "CLICK_USER_LOGGED",
             payload: response.data,
           });
         } catch (error) {
