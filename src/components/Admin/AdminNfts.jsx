1cbbdcabfdd280ec07c60d53101abe7526fa9d7b
¨hbonavota¨ <hbonavota@gmail.com>
2021-09-21 18:08:37 +0200

    <add styles>[admin]

--- src/components/Admin/AdminNfts.jsx
+++ src/components/Admin/AdminNfts.jsx
@@ -1,69 +1,67 @@
-import React, { useEffect,useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { getNFTs } from '../../actions/getNFTs'
-import {deleteNFT} from '../../actions/admin/deleteNFT'
+import { deleteNFT } from '../../actions/admin/deleteNFT'
 import { Link } from 'react-router-dom'
 import { Button } from '@material-ui/core'
 
-export default function Admi0Nfts() {   
-    const dispatch = useDispatch();
-    const nfts=useSelector(state=>state.allNFTs)
-  
-    useEffect(() => {
-        dispatch(getNFTs())
-    }, [dispatch])
+export default function Admi0Nfts() {
+  const dispatch = useDispatch();
+  const nfts = useSelector(state => state.allNFTs)
 
-    const [inputs,setInputs]=useState({
-        deleteNFT: []
-      })
-    
-    function onInputChange(e) {
-       e.preventDefault()
-       setInputs({
-           ...inputs,
-           [e.target.name]: inputs[e.target.name].concat(e.target.value)
-          });
-         
-      }
-      
-    async function handleDeleteNFT(e) {
-        e.preventDefault()
-        dispatch(deleteNFT(inputs.deleteNFT))
-        alert('NFT deleted')
-        dispatch(getNFTs())
-        setInputs({deleteNFT:['']})
-        
-      } 
-     
-    
-    return ( <div> 
-         <Link to='/admin'>
-         <Button variant='contained' size='large'>Back</Button> 
-      </Link>          
-             <h2>NFTs</h2>
-             <form name="deleteNFT" onSubmit={(e)=>handleDeleteNFT(e)}>
-            <h3>Delete NFT</h3>
-            <label htmlFor="">NFTs</label>             
+  useEffect(() => {
+    dispatch(getNFTs())
+  }, [dispatch])
+
+  const [inputs, setInputs] = useState({
+    deleteNFT: []
+  })
+
+  function onInputChange(e) {
+    e.preventDefault()
+    setInputs({
+      ...inputs,
+      [e.target.name]: inputs[e.target.name].concat(e.target.value)
+    });
+
+  }
+
+  async function handleDeleteNFT(e) {
+    e.preventDefault()
+    dispatch(deleteNFT(inputs.deleteNFT))
+    alert('NFT deleted')
+    dispatch(getNFTs())
+    setInputs({ deleteNFT: [''] })
+
+  }
+
+
+  return (<div>
+    <Link to='/admin'>
+      <Button variant='contained' size='large'>Back</Button>
+    </Link>
+    <h2>NFTs</h2>
+    <form name="deleteNFT" onSubmit={(e) => handleDeleteNFT(e)}>
+      <h3>Delete NFT</h3>
+      <label htmlFor="">NFTs</label>
+      <div>
+        {nfts.map((n) => (
+          <div key={n._id}>
+            <input
+              type="checkbox"
+              name="deleteNFT"
+              value={n._id}
+              onChange={(e) => onInputChange(e)}
+            ></input>
             <div>
-              {nfts.map((n) => (
-                <div key={n._id}>
-                  <input
-                    type="checkbox"
-                    name="deleteNFT"
-                    value={n._id}
-                    onChange={(e)=>onInputChange(e)}
-                    ></input>
-                    <div>
-                  <label name={n.name}> {n.name} </label>
-                  <img src={n.image} alt="NFT image" width="60" height="60"/>
-                  </div>
-                </div>
-              ))}
-            </div>            
-           <button type="submit">Delete!</button>
-            </form>
-        
-        </div>
-
-      )
-    }
+              <label name={n.name}> {n.name} </label>
+              <img src={n.image} alt="NFT image" width="60" height="60" />
+            </div>
+          </div>
+        ))}
+      </div>
+      <Button variant='contained' size='large' type="submit">Delete!</Button>
+    </form>
+  </div>
+  )
+}
--- src/components/Admin/HomeAdmin.jsx
+++ src/components/Admin/HomeAdmin.jsx
@@ -9,7 +9,6 @@ export default function HomeAdmin() {
             <Box mb='1rem'>
                 <h3>Admin</h3>
                 <Link to='admin/users'><Button variant='contained' size='large' >Users</Button></Link>
-                
                 <Link to='admin/nfts'> <Button variant='contained' size='large' >Nfts</Button></Link>
                 <Link to='admin/categories'><Button variant='contained' size='large' >Categories</Button></Link>
             </Box>
@@ -17,9 +16,3 @@ export default function HomeAdmin() {
     </div>
     )
 }
-
-{/* <Box mb='1rem'>
-    <Button type='submit' variant='outlined' size='large' fullWidth>
-        Send
-    </Button>
-</Box> */}
