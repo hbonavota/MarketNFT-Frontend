b1f6b0ea127f8db879df43e965de719baeca9991
Agustina Corbalan <tinicorb@gmail.com>
2021-09-23 22:01:27 -0300

    Admin fixed

--- src/components/Admin/AdminUser.jsx
+++ src/components/Admin/AdminUser.jsx
@@ -1,31 +1,30 @@
-import React, { useEffect, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import usersToAdmin from '../../actions/admin/usersToAdmin'
-import { getUsers } from '../../actions/admin/getUsers';
-import { Link } from 'react-router-dom';
-import { deleteUser } from '../../actions/admin/deleteNFT'
-import { Button } from '@material-ui/core'
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import usersToAdmin from "../../actions/admin/usersToAdmin";
+import { getUsers } from "../../actions/admin/getUsers";
+import { Link } from "react-router-dom";
+import { deleteUser } from "../../actions/admin/deleteNFT";
+import { Button } from "@material-ui/core";
 
 export default function AdminUser() {
   const dispatch = useDispatch();
-  const users = useSelector(state => state.allUsers)
+  const users = useSelector((state) => state.allUsers);
 
   useEffect(() => {
-    dispatch(getUsers())
-  }, [dispatch])
+    dispatch(getUsers());
+  }, [dispatch]);
 
   const [inputs, setInputs] = useState({
     deleteUser: [],
-    users: []
-  })
+    users: [],
+  });
 
   function onInputChange(e) {
-    e.preventDefault()
+    e.preventDefault();
     setInputs({
       ...inputs,
-      [e.target.name]: inputs[e.target.name].concat(e.target.value)
+      [e.target.name]: inputs[e.target.name].concat(e.target.value),
     });
-
   }
   async function handleDeleteUser(e) {
     e.preventDefault();
@@ -35,67 +34,69 @@ export default function AdminUser() {
     setInputs({ deleteUser: [""] });
   }
 
-
   async function handleRole(e) {
-    e.preventDefault()
-    dispatch(usersToAdmin(inputs.users))
-    alert('Role changed')
-    dispatch(getUsers())
-    setInputs({ users: [''] })
-
+    e.preventDefault();
+    dispatch(usersToAdmin(inputs.users));
+    alert("Role changed");
+    dispatch(getUsers());
+    setInputs({ users: [""] });
   }
 
+  return (
+    <div>
+      <Link to="/admin">
+        <Button variant="contained" size="large">
+          Back
+        </Button>
+      </Link>
+      <h2>Users</h2>
 
-
-  return (<div>
-    <Link to='/admin'>
-      <Button variant='contained' size='large'>Back</Button>
-    </Link>
-    <h2>Users</h2>
-
-    <form name="users"
-      onSubmit={(e) => handleRole(e)}
-    >
-      <h3>Change role</h3>
-      <label htmlFor="">Users To Admin</label>
-      <div>
-        {users.map((u) => (
-          <div key={u._id}>
-            <input
-              type="checkbox"
-              name="users"
-              value={u.username}
-              onChange={(e) => onInputChange(e)}
-            ></input>
-            <div>
-              <label name={u.firstName}> {u.firstName}- {u.username} </label>
+      <form name="users" onSubmit={(e) => handleRole(e)}>
+        <h3>Change role</h3>
+        <label htmlFor="">Users To Admin</label>
+        <div>
+          {users?.map((u) => (
+            <div key={u._id}>
+              <input
+                type="checkbox"
+                name="users"
+                value={u.username}
+                onChange={(e) => onInputChange(e)}
+              ></input>
+              <div>
+                <label name={u.firstName}>
+                  {" "}
+                  {u.firstName}- {u.username}{" "}
+                </label>
+              </div>
             </div>
-          </div>
-        ))}
-      </div>
-      <Button type="submit" variant='contained' size='large'>Change!</Button>
-    </form>
+          ))}
+        </div>
+        <Button type="submit" variant="contained" size="large">
+          Change!
+        </Button>
+      </form>
 
-    <form onSubmit={(e) => handleDeleteUser(e)}>
-      <h3>Delete User</h3>
-      <label htmlFor="">Users</label>
-      <select
-        name="deleteUser"
-        onChange={(e) => onInputChange(e)}
-        defaultValue=""
-      >
-        <option value="">Choose User</option>
-        {users.map((u) => (
-          <option key={u._id} name={u._id} value={u._id}>
-            {" "}
-            {u.username}{" "}
-          </option>
-        ))}
-      </select>
-      <Button type="submit" variant='contained' size='large'>Delete User!</Button>
-    </form>
-
-
-  </div>
-  )
+      <form onSubmit={(e) => handleDeleteUser(e)}>
+        <h3>Delete User</h3>
+        <label htmlFor="">Users</label>
+        <select
+          name="deleteUser"
+          onChange={(e) => onInputChange(e)}
+          defaultValue=""
+        >
+          <option value="">Choose User</option>
+          {users.map((u) => (
+            <option key={u._id} name={u._id} value={u._id}>
+              {" "}
+              {u.username}{" "}
+            </option>
+          ))}
+        </select>
+        <Button type="submit" variant="contained" size="large">
+          Delete User!
+        </Button>
+      </form>
+    </div>
+  );
 }
