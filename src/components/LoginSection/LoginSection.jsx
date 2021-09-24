546c2c92944d1376a97205b88b0798387ce6c2c0
anaharri <anaharringtonb@gmail.com>
2021-09-24 00:16:29 -0300

    setTimeout para la alerta del login

--- src/components/LoginSection/LoginSection.jsx
+++ src/components/LoginSection/LoginSection.jsx
@@ -27,7 +27,7 @@ export default function LoginSection() {
     dispatch(action(state))
 
     if (!userLogged) {
-      dispatch(alert(true))
+      setTimeout(dispatch(alert(true)), 2000)
     } else {
       dispatch(alert(false))
       history.push('/')
