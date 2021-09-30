3d8863c166d8119bde211a10a018bca64e0ff4f7
Daniel Mojica <drmc47@gmail.com>
2021-09-29 22:52:57 -0300

    metamask arreglado

--- src/actions/shoppingCart/removeItem.js
+++ src/actions/shoppingCart/removeItem.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export default function removeItem(payload) {
     return async function (dispatch) {
       try {
-        let response = await axios.delete("/deleteItem/",payload);
+        let response = await axios.post("/deleteItem/",payload);
          return dispatch({
             type: "DB_SHOPPING_CART",
             payload: response.data,
